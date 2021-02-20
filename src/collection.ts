f2e862149c6a4d53fa6b49daabb323d13f8355d7
muchan92 <muchan92@qq.com>
2021-02-20 17:52:01 +0800

    perf(carray): prototype
    Signed-off-by: muchan92 <muchan92@qq.com>

--- src/collection.ts
+++ src/collection.ts
@@ -9,41 +9,29 @@ import { typedef, change, TypeDesc } from './type'
 
 import { array } from './builtin'
 
-const refMap = new WeakMap(),
-	protoCache = new WeakMap()
+const refMap = new WeakMap()
 
 const syl_raw = Symbol(),
 	syl_CArray = Symbol()
 
-const arrayKeys = {
-	push: 1,
-	pop: 1,
-	shift: 1,
-	unshift: 1,
-	splice: 1,
-	sort: 1,
-	reverse: 1,
-}
+const CArrayProto: any = {}
+;['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'].forEach(
+	(key) => {
+		CArrayProto[key] = function (...args: any[]) {
+			const ret = this[syl_raw][key](...args)
+			change(this)
+			return ret
+		}
+	}
+)
 const CArrayHandler = {
 	get(target: any, key: string | number | symbol, receiver: any) {
 		if (key === syl_raw) {
 			return target
 		}
-		if (key in arrayKeys) {
-			const proto = Object.getPrototypeOf(target)
-			let p = protoCache.get(proto)
-			if (!p) {
-				p = {}
-				protoCache.set(proto, p)
-			}
-			if (!p[key]) {
-				p[key] = function (...args: any[]) {
-					const ret = this[syl_raw][key](...args)
-					change(this)
-					return ret
-				}
-			}
-			return p[key]
+		const f = CArrayProto[key]
+		if (f) {
+			return f
 		}
 		return Reflect.get(target, key, receiver)
 	},
